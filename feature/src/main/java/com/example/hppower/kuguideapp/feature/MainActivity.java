d2834a56c1f409ba189c8adf7720e7ab11f426d6
HP POWER <bhabukunwar@gmail.com>
2018-12-24 14:09:15 +0545

    first

---
+++ feature/src/main/java/com/example/hppower/kuguideapp/feature/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.hppower.kuguideapp.feature;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonForMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        buttonForMap = findViewById(R.id.mapButton);
+        buttonForMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
